d06b806b75bdb9d1023e1f258f40c128404581ee
wided msakem <widedmsakem@gmail.com>
2024-11-05 11:32:50 +0100

    dojo_survey_validation

--- python/week 2/day 5/practice/dojo_survey_assignment/flask_app/controllers/dojos.py
+++ python/week 2/day 5/practice/dojo_survey_assignment/flask_app/controllers/dojos.py
@@ -1,9 +1,29 @@
 from flask_app import app
-from flask import render_template,redirect,request
-from flask_app.models.users_model import Dojos
+from flask import render_template,redirect,request,flash
+from flask_app.models.dojos_model import Dojos
 
 
 
 @app.route('/')
 def create_dojo():
-    return render_template('dojo_index.html')
+    return render_template('dojos_index.html')
+
+
+@app.route('/dojo/create',methods=['POST'])
+def save_to_db():
+    
+    if not Dojos.validate_data(request.form) :
+        
+        return redirect('/')
+    else:
+        dojo_id = Dojos.save(request.form)
+        return redirect('/result/'+str(dojo_id))
+
+
+@app.route('/result/<int:dojo_id>')
+def display_dojo(dojo_id):
+    dojo=Dojos.get_one({'id':dojo_id})
+    print(dojo)
+    return render_template('result.html', dojo=dojo)
+
+
--- python/week 2/day 5/practice/dojo_survey_assignment/flask_app/models/dojos_model.py
+++ python/week 2/day 5/practice/dojo_survey_assignment/flask_app/models/dojos_model.py
@@ -1,9 +1,10 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import DB
+from flask import flash
 
 
 
-class Users :
+class Dojos :
     def __init__(self,data):
         self.id=data['id']
         self.name=data['name']
@@ -17,7 +18,7 @@ class Users :
 
     @classmethod 
     def get_all(cls):
-        query = "SELECT * FROM users ;"
+        query = "SELECT * FROM dojo ;"
         results=connectToMySQL(DB).query_db(query)
         dojos=[]
         for row in results :
@@ -36,7 +37,7 @@ class Users :
 
     @classmethod
     def save(cls,data):
-        query="INSERT INTO dojo (name,location,favorite_language,comment) VALUES (%(name)s,%(location)s,%(favorite_langauge)s,%(comment)s ;"
+        query="INSERT INTO dojo (name,location,favorite_language,comment) VALUES (%(name)s,%(location)s,%(favorite_language)s,%(comment)s) ;"
         results= connectToMySQL(DB).query_db(query,data)
         return results 
 
@@ -50,3 +51,27 @@ class Users :
     def delete(clas,data) :
         query="DELETE FROM dojo WHERE id=%(id)s ;"
         return  connectToMySQL(DB).query_db(query,data)
+
+
+
+
+    @staticmethod
+    def validate_data(data) :
+        name=data['name']
+        location=data['location']
+        favorite_language=data['favorite_language']
+        comment=data['comment']
+        is_valid=True
+        if len(name) < 3 :
+            is_valid=False 
+            flash("Name must contain at least 3 characters ","name_validation")
+        if not (location) :
+            is_valid=False
+            flash("You must select a location","location_validation")
+        if not (favorite_language) :
+            is_valid=False
+            flash("You must select a language","favorite_language_validation")
+        if len(comment)< 3 :
+            is_valid=False
+            flash("Comment section must contain at least 3 charactaers","comment_validation")
+        return is_valid

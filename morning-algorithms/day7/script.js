9eaf63a35ffbe3796710991901789b425db031b8
wided msakem <widedmsakem@gmail.com>
2024-11-06 10:29:08 +0100

    email_validation_assignment

--- morning-algorithms/day7/script.js
+++
@@ -1,7 +0,0 @@
-/* steps 
-our function's goal to iterate through an array of numbers and return the element that is not repeated 
-1- first step is to check whether the array entered by the user is empty or not , if so , the function will automatically return null 
-2- otherwise , we are going to create a for loop  that starts from 0 till the end of the array(arr.length-1) that will iterate 
-through the array inside the loop itself we are going to create another loop with the same parametr of the first loop its role is to 
-keep track of the iterated elements of the array and store their values    
-/* 
